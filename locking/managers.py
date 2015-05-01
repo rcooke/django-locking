c4c7783812070d8a86911f76a7fbba140ccfc252
James Aylett <james@tartarus.org>
2015-05-01 11:50:22 +0100

    Drop Django 1.5 support.

--- locking/managers.py
+++ locking/managers.py
@@ -22,14 +22,8 @@ class LockedManager(Manager):
         timeout = point_of_timeout()
         return super(LockedManager, self).get_query_set().filter(_locked_at__gt=timeout, _locked_at__isnull=False)
 
-    if django.VERSION < (1, 6):
-        get_query_set = get_queryset
-
 
 class UnlockedManager(Manager):
     def get_queryset(self):
         timeout = point_of_timeout()
         return super(UnlockedManager, self).get_query_set().filter(Q(_locked_at__lte=timeout) | Q(_locked_at__isnull=True))
-
-    if django.VERSION < (1, 6):
-        get_query_set = get_queryset
