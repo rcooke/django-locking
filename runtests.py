b17a39ae6489916868a65f19c80a42ac22531909
James Aylett <james@tartarus.org>
2015-05-01 11:49:35 +0100

    Simple test runner, Django 1.6+.

---
+++ runtests.py
@@ -0,0 +1,30 @@
+import os.path
+import subprocess
+import sys
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    INSTALLED_APPS=[
+        'django.contrib.contenttypes',
+        'django.contrib.auth',
+        'locking',
+    ],
+    SECRET_KEY='empty',
+    LOCKING={
+        'time_until_expiration': 120,
+        'time_until_warning': 60
+    },
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'test',
+        },
+    },
+)
+
+from django.test.runner import DiscoverRunner
+test_runner = DiscoverRunner(verbosity=1, failfast=False)
+failures = test_runner.run_tests(['locking', ])
+if failures: #pragma no cover
+    sys.exit(failures)
