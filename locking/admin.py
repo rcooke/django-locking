322b125aefa3a2fd3fd24f5e4e5d823e7221b9ec
Richard Cooke <rcooke@tnky.ca>
2016-09-01 11:10:39 -0400

    Internal variable name change in Django 1.8
        As noted here:
        https://code.djangoproject.com/ticket/20853
    
        The name of an internal variable has changed in recent Django versions.
        This change was needed for Django 18.

--- locking/admin.py
+++ locking/admin.py
@@ -46,7 +46,9 @@ class LockableAdminMixin(object):
 
     def locking_media(self, obj=None):
         opts = self.model._meta
-        info = (opts.app_label, opts.module_name)
+# https://code.djangoproject.com/ticket/20853
+#        info = (opts.app_label, opts.module_name)
+        info = (opts.app_label, opts.model_name)
         pk = getattr(obj, 'pk', None) or 0
         return forms.Media(js=(
             reverse('admin:%s_%s_lock_js' % info, args=[pk]),))
@@ -74,7 +76,9 @@ class LockableAdminMixin(object):
             return functools.update_wrapper(wrapper, view)
 
         opts = self.model._meta
-        info = (opts.app_label, opts.module_name)
+# https://code.djangoproject.com/ticket/20853
+#        info = (opts.app_label, opts.module_name)
+        info = (opts.app_label, opts.model_name)
 
         urlpatterns = patterns('',
             url(r'^(.+)/locking_variables\.js$',
