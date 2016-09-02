11de14bcc00130b12dc366692e726e787f8407dd
Richard Cooke <rcooke@tnky.ca>
2016-09-02 13:56:44 -0400

    Setup must specify a static string version.
    As suspected, you can't import the package version for a package
    before you have installed the package!
    
    If the package was not installed before, it uses 0,0,0.
    Otherwise, I suspect it would re-use the old package's version.

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ long_description = open(README).read()
 
 setup(
     name='django-locking',
-#    version='2.2.19',
-    version=__import__('locking').__version__,
+    version='2.2.19',
     description=("Prevents users from doing concurrent editing in Django. Works out of the box in the admin interface, or you can integrate it with your own apps using a public API."),
     long_description=long_description,
     classifiers=['Development Status :: 4 - Beta',
