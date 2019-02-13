1b06adca0a4e0b78088d15d159c82fb00235f301
Richard Cooke <rcooke@tnky.ca>
2019-02-13 11:39:10 -0500

    Fixed first test constant, and noted source so it is easier to find

--- locking/tests/tests.py
+++ locking/tests/tests.py
@@ -10,5 +10,5 @@ class ExpirationTestCase(TestCase):
     def test_lock_seconds_remaining(self):
         lock = LockFactory(_locked_at=timezone.now())
         actual = lock.lock_seconds_remaining
-        expected = 120 # as set in runtests.py
-        self.assertAlmostEqual(expected, actual, delta=1)
+        expected = 600 # as set in locking.setting.TIME_UNTIL_EXPIRATION
+        self.assertAlmostEqual(expected, actual, delta=3)
