7c85e709dbe90c928115e339155be659ed9ebf43
Richard Cooke <rcooke@tnky.ca>
2019-02-13 11:52:43 -0500

    Bumping to Rev 2.3.1 to avoid conflict with really old tag

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open(README).read()
 
 setup(
     name='django-locking',
-    version='2.3.0',
+    version='2.3.1',
     description=("Prevents users from doing concurrent editing in Django. Works out of the box in the admin interface, or you can integrate it with your own apps using a public API."),
     long_description=long_description,
     classifiers=['Development Status :: 4 - Beta',
